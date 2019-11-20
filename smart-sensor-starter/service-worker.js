ccbe643c0d97e0c7ab2652499c4ee2d8197f97e8
hunsalz <hunsalz@users.noreply.github.com>
2019-11-20 15:36:26 +0000

    hunsalz published a site update

--- smart-sensor-starter/service-worker.js
+++ smart-sensor-starter/service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "3185b9cbbf3944ae8b80927edf4bf3cd"
+    "revision": "b91ae880d21421e29641242e7baffe18"
   },
   {
     "url": "404/index.html",
-    "revision": "3185b9cbbf3944ae8b80927edf4bf3cd"
+    "revision": "b91ae880d21421e29641242e7baffe18"
   },
   {
     "url": "assets/css/0.styles.d830eec0.css",
@@ -39,47 +39,47 @@ self.__precacheManifest = [
   },
   {
     "url": "assets/data/404/index.json",
-    "revision": "708d3665a44785dea118cce53446cd49"
+    "revision": "0b323974d6976892fe8ee9c5c9cb44e3"
   },
   {
     "url": "assets/data/index.json",
-    "revision": "718125be5c5977c508ce10ad124d3b3e"
+    "revision": "420f2218dc93cea3d2f4a72c45412651"
   },
   {
     "url": "assets/data/kitchen/index.json",
-    "revision": "1a9a2f833e6f758bdaa85953b0dc3024"
+    "revision": "04f66c82217072f5fe31b155e2dfa7af"
   },
   {
     "url": "assets/data/markdown-test-file/index.json",
-    "revision": "6647ed47d98adeab1ec3a3757d1195d2"
+    "revision": "f38b4e2e822a6d538c916522e61852f1"
   },
   {
     "url": "assets/data/say-hello-to-gridsome/index.json",
-    "revision": "30b8bcda5ac9f9ef01b13054436d5142"
+    "revision": "d498c21dccbb2c4579394769835f460d"
   },
   {
     "url": "assets/data/tag/Humidity/index.json",
-    "revision": "9ae4ea31eaf18e513aaba95fcb6e9aca"
+    "revision": "2f2a04cb86fee69d71b832b96edf84b1"
   },
   {
     "url": "assets/data/tag/Markdown/index.json",
-    "revision": "d8c0ef3c1d59233a505073b1ad802cfb"
+    "revision": "a5fa872c7b9990ee59a2a15915a36630"
   },
   {
     "url": "assets/data/tag/Releases/index.json",
-    "revision": "ac1d936eb2e7ca0be430f1fc73ebaffb"
+    "revision": "18163bfd6559bf54067eacf8790fdfdf"
   },
   {
     "url": "assets/data/tag/Temperature/index.json",
-    "revision": "3b2e2ad78ea1f434934aa5b723f9b01e"
+    "revision": "4a010bc8650a47323ccbd37902fa0e44"
   },
   {
     "url": "assets/data/tag/Test files/index.json",
-    "revision": "94fee9f61429083f6936f5bcd5fefa2f"
+    "revision": "437115ca0e6a70f67d2b702630594fac"
   },
   {
-    "url": "assets/js/app.55298366.js",
-    "revision": "356c9c316d7405d472551623c09f3544"
+    "url": "assets/js/app.73ef96b1.js",
+    "revision": "bee6a2a3fc8354b1797282da162c7745"
   },
   {
     "url": "assets/js/page--node-modules--gridsome--app--pages--404-vue.6e92453a.js",
@@ -167,43 +167,43 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "1e9f57c27c2971b68ef4b0028d104d9b"
+    "revision": "399efa6c2efbd2177e6ad1d051a9d666"
   },
   {
     "url": "kitchen/index.html",
-    "revision": "3f19d1fb9e8ebe274dace2ce079c0966"
-  },
-  {
-    "url": "manifest.json",
-    "revision": "9850f1879b51766d062de6c3ab3c4585"
+    "revision": "378512098869785bd889b479f7e4b752"
   },
   {
     "url": "markdown-test-file/index.html",
-    "revision": "776c1188ea9cde2b8affed9aa1f47521"
+    "revision": "5f36ae0806a02e153a89fee33396f70c"
   },
   {
     "url": "say-hello-to-gridsome/index.html",
-    "revision": "d95fbbf3cd092d9892d466ee9df38bdc"
+    "revision": "458a6c547c5cc3b61f01a38fcecffe47"
+  },
+  {
+    "url": "smart-sensor-starter/manifest.json",
+    "revision": "9850f1879b51766d062de6c3ab3c4585"
   },
   {
     "url": "tag/Humidity/index.html",
-    "revision": "b499e7d74a332937243387dbe5e0e8b2"
+    "revision": "2e4c45a39f995a15d2d0474aa557e302"
   },
   {
     "url": "tag/Markdown/index.html",
-    "revision": "40d70253c4ff0f33e187dcb9995c5491"
+    "revision": "18bbe28d75ce5ef30b9b1dc7fe48045f"
   },
   {
     "url": "tag/Releases/index.html",
-    "revision": "d1c72221aee275c42c1e581300d605eb"
+    "revision": "df1ce2fc1e34b65bd8cb7bfbd07fa96a"
   },
   {
     "url": "tag/Temperature/index.html",
-    "revision": "f6f16728b7796b36401b3af9f15779b4"
+    "revision": "22236203207dc8adfb3407268bc0b680"
   },
   {
     "url": "tag/Test files/index.html",
-    "revision": "5360034678f0548777c164bd634034cc"
+    "revision": "cc3b841328268288956f361b12529095"
   },
   {
     "url": "404",
