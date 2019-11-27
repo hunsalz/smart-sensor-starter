aa5e98c053ac1ccf75779b9d04c8f383c7038396
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2019-11-27 22:09:02 +0100

    added dev mode for debugginh

--- gridsome.server.js
+++ gridsome.server.js
@@ -9,4 +9,7 @@ module.exports = function (api) {
   api.loadSource(({ addCollection }) => {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api/
   })
+  api.chainWebpack(config => {
+    config.mode('development')
+  })  
 }
