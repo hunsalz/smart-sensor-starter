dffa8f1d4563ce39b2a874e328ae964de907d6cd
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2019-11-29 22:42:57 +0100

    --wip--

--- src/components/SensorCard.vue
+++ src/components/SensorCard.vue
@@ -14,13 +14,11 @@
     <div class="sensor-card__content">
       <h2 class="sensor-card__title" v-html="sensor.title" />
       <p class="sensor-card__content" v-html="sensor.content" />
-<!--       <p>
-        <SensorValues class="sensor-card__meta" :sensor="sensor" />
-      </p>
+
 
       <TagList class="sensor-card__tags" :sensor="sensor" />
 
-      <div class="meta-info">Last updated at {{ sensor.date }}</div> -->
+      <div class="meta-info">Last updated at {{ sensor.date }}</div>
 
       <g-link class="sensor-card__link" :to="sensor.path">Link</g-link>
     </div>
