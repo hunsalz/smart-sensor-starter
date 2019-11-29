a45a009ba18dcfe3920c4f0cb83b9fdb0866a888
hunsalz <hunsalz@users.noreply.github.com>
2019-11-29 08:58:26 +0000

    hunsalz published a site update

--- service-worker.js
+++ service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "c18fc5bf7a2025713f245b31f97b6b89"
+    "revision": "31f893bedd08d8befcdcce29bf5d38ca"
   },
   {
     "url": "404/index.html",
-    "revision": "c18fc5bf7a2025713f245b31f97b6b89"
+    "revision": "31f893bedd08d8befcdcce29bf5d38ca"
   },
   {
     "url": "assets/css/0.styles.378d53fe.css",
@@ -39,55 +39,55 @@ self.__precacheManifest = [
   },
   {
     "url": "assets/data/404/index.json",
-    "revision": "7ac6f06cffa7d63a0ff1ec4d30761403"
+    "revision": "8758a1484ff6a21f78581c72f2897ad0"
   },
   {
     "url": "assets/data/garden/index.json",
-    "revision": "c774fc7d415d5176470d33c7f934498a"
+    "revision": "622d98de100e98ba2a784260daa88c1b"
   },
   {
     "url": "assets/data/index.json",
-    "revision": "3fa5a1c963abea141a0b9db1bdb7b5f3"
+    "revision": "80c5144d585a4e29e8f7baf25a48d0d1"
   },
   {
     "url": "assets/data/kitchen/index.json",
-    "revision": "08d9842316204da3d3563171b3c6e291"
+    "revision": "20eac9eb2e575217b59c2f9297fbe9ac"
   },
   {
     "url": "assets/data/office/index.json",
-    "revision": "0c3a5a988b9566342cbbca34196e536b"
+    "revision": "401c2eb4b9e6016d90ac9051fe1d00bc"
   },
   {
     "url": "assets/data/tag/Altitude/index.json",
-    "revision": "f276327f7cdd7aae7c1ee0626d6e95bd"
+    "revision": "98a5c48fbc968262a840a1cfe0d11f2f"
   },
   {
     "url": "assets/data/tag/Humidity/index.json",
-    "revision": "cdd7368a6c919744dff1e3db1eb347d9"
+    "revision": "482b35a1985d085f4575b33af10ad462"
   },
   {
     "url": "assets/data/tag/Temperature/index.json",
-    "revision": "78f466f912b7f28a5688e3933be2b5c9"
+    "revision": "7d14da934efa46b07b3b15eb9c17e907"
   },
   {
-    "url": "assets/js/app.4d79f062.js",
-    "revision": "df3f43a0c8390d35e81e39b9d8e2b3b9"
+    "url": "assets/js/app.50c5afb9.js",
+    "revision": "e944d4b2d1c5ab72c3d087da1faee02d"
   },
   {
     "url": "assets/js/page--node-modules--gridsome--app--pages--404-vue.68fe12b0.js",
     "revision": "f09cbcbea7e9381983660ad60cea6f1a"
   },
   {
-    "url": "assets/js/page--src--pages--index-vue.8e9e0b23.js",
-    "revision": "7aa4e04d8f5d5fdceba56d9f4aa171bc"
+    "url": "assets/js/page--src--pages--index-vue.c66eadd6.js",
+    "revision": "6d52dd1ad19f05c9eff958c91eb33420"
   },
   {
     "url": "assets/js/page--src--templates--sensor-vue.fa619ee2.js",
     "revision": "b6b4bc5e317f9f25f93711bbeca497f0"
   },
   {
-    "url": "assets/js/page--src--templates--tag-vue.7bef9e39.js",
-    "revision": "07833dc251023d4251c2e31b1b03017d"
+    "url": "assets/js/page--src--templates--tag-vue.cd964380.js",
+    "revision": "074a1c7a4c546dbe0c8b04b2b3e6a614"
   },
   {
     "url": "assets/static/esp32.07cc2b7.5f26e714a295fea020dbdff12a80a332.jpeg",
@@ -139,15 +139,15 @@ self.__precacheManifest = [
   },
   {
     "url": "garden/index.html",
-    "revision": "805f2e76dcf45308d8e06520f3543a6c"
+    "revision": "3ef34aaaebf422a96403781f652f7bb8"
   },
   {
     "url": "index.html",
-    "revision": "92738c4e79f77ebb3e967900ce850d6d"
+    "revision": "1fae7a2d9f017e4fe8b8d5c13edcfc23"
   },
   {
     "url": "kitchen/index.html",
-    "revision": "45fa0fcced41a5d97e868b305f3f9954"
+    "revision": "a2660b7c0efccfdd8cebe6af259bc761"
   },
   {
     "url": "manifest.json",
@@ -155,19 +155,19 @@ self.__precacheManifest = [
   },
   {
     "url": "office/index.html",
-    "revision": "1816efd10a33e096e82b2063eb30b07f"
+    "revision": "d0aded9594f7497c39e1ebb29bcdf2cd"
   },
   {
     "url": "tag/Altitude/index.html",
-    "revision": "09f73a3b4f5e7005b19d4e035a93553d"
+    "revision": "921a06c287695a0fe4e1a8614906d3e9"
   },
   {
     "url": "tag/Humidity/index.html",
-    "revision": "1f2bf2954577f0aa9e42b7347b311cb8"
+    "revision": "c68bae49f12cceb007614be08f76faa8"
   },
   {
     "url": "tag/Temperature/index.html",
-    "revision": "ffaec081a47573a02c141558dd97dd1d"
+    "revision": "a2fff069e55d3830765854e69ddfcaa3"
   },
   {
     "url": "404",
@@ -186,12 +186,12 @@ self.__precacheManifest = [
     "revision": "7f3ea71b1bf36c086397d84f28eb78a3"
   },
   {
-    "url": "kitchen",
-    "revision": "00d96ba4a29deb50d190184bcb516747"
-  },
-  {
     "url": "garden",
     "revision": "5c4bb96b75c4a24cdd2909e0a5d9951c"
+  },
+  {
+    "url": "kitchen",
+    "revision": "00d96ba4a29deb50d190184bcb516747"
   },
   {
     "url": "office",
