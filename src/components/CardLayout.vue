7d3d4489d84c34f39e0caf3e6f3d5edf2b1416dc
Markus Hunsalz <markushunsalz@Markuss-MBP.fritz.box>
2019-12-04 08:07:26 +0100

    fixed g-link

--- src/components/CardLayout.vue
+++ src/components/CardLayout.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="card-layout content-box">
-    <div class="card-layout__header">
-      <g-image
-        class="card-layout__image"
-        v-if="entry.cover_image"
-        :src="entry.cover_image"
-        alt="Cover image"
-      />
-    </div>
-    <div class="card-layout__main">
-      <h2 class="card-layout__title" v-html="entry.title" />
-      <BoxLayout :entry="entry" />
-      <TagList class="card-layout__tags" :entry="entry" />
-      <g-link class="card-layout__link" :to="entry.path">Link</g-link>
-    </div>
+    <g-link class="card-layout__link" :to="entry.path">
+      <div class="card-layout__header">
+        <g-image
+          class="card-layout__image"
+          v-if="entry.cover_image"
+          :src="entry.cover_image"
+          alt="Cover image"
+        />
+      </div>
+      <div class="card-layout__main">
+        <h2 class="card-layout__title" v-html="entry.title" />
+        <BoxLayout :entry="entry" />
+        <TagList class="card-layout__tags" :entry="entry" />
+      </div>
+    </g-link>
   </div>
 </template>
 
@@ -32,7 +33,6 @@ export default {
 
 <style lang="scss">
 .card-layout {
-
   &__header {
     border-radius: var(--radius) var(--radius) 0 0;
     overflow: hidden;
@@ -61,14 +61,8 @@ export default {
   }
 
   &__link {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0;
-    overflow: hidden;
-    text-indent: -9999px;
+    text-decoration: none;
+    color: currentColor;
     z-index: 0;
   }
 }
